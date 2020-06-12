e91d827654895fd0e3edbe7e5683f582e2fe1dcd
megan-pg <megan.pardygokcu@unh.edu>
2020-06-12 18:02:42 -0400

    set up apis and base code for files

---
+++ public/assets/js/index.js
@@ -0,0 +1,30 @@
+
+const getNotes = //$AJAX //method
+
+//apinotes will be the url
+//GET method will be the key
+
+// const saveNotes 
+
+//apinotes will be the url
+//GET method will be the key
+
+//date will just be note
+
+//const delete 
+
+//add id here
+//method will be DELETE
+
+//funtion activeNote
+
+//if there is an active note, display , otherwise empty imputs
+
+//function savingTheNote
+
+//save title of note, text of note to database
+
+//function deletedNote
+
+//figure out which note is deleted and remove from database
+
